bfa439ffe303cb88c8480d63aec5422894b16501
pablonq <pablo.navarro@est.fi.uncoma.edu.ar>
2025-04-07 16:07:06 -0300

    metodos

---
+++ metodos_cadena.py
@@ -0,0 +1,59 @@
+dato = "hola estoy estudiando python"
+dato2 = 5
+dato3 = 5.3
+
+
+#Dir: Devuelve una lista con los nombres de todos los métodos y atributos del objeto
+dir(dato)
+# print(dir(dato))
+
+#upper: Devuelve una cadena con todos los caracteres en mayúscula
+dato.upper()
+# print(dato.upper())
+
+#lower: Devuelve una cadena con todos los caracteres en minúscula
+dato.lower()
+
+
+#capitalize: Devuelve una cadena con el primer carácter en mayúscula y el resto en minúscula
+dato.capitalize()
+
+#find: Devuelve el índice del primer carácter de la cadena
+dato.find("h")
+
+#index: Devuelve el índice del primer carácter de la cadena
+dato.index("h")
+
+#isnumeric: Devuelve True si todos los caracteres de la cadena son numéricos
+dato.isnumeric()
+
+#isalpha: Devuelve True si todos los caracteres de la cadena son alfabéticos
+dato.isalpha()
+
+#count: Cuenta el número de veces que aparece una cadena
+dato.count("h")
+
+#len: Devuelve el número de caracteres de la cadena
+dato.len()
+
+#endswith: Devuelve True si la cadena termina con una cadena
+dato.endswith("python")
+
+#startswith: Devuelve True si la cadena comienza con una cadena
+dato.startswith("hola")
+
+#replace: Reemplaza una cadena por otra
+dato.replace("hola", "hola2")
+
+#split: Divide la cadena en una lista de cadenas
+dato.split(" ")
+
+#strip: Elimina los espacios en blanco al inicio y al final de la cadena
+dato.strip()
+
+#join: Une una lista de cadenas en una cadena
+dato.join(["hola", "estoy", "estudiando", "python"])
+
+#format: Formatea una cadena
+
+#
---
+++ metodos_lista.py
@@ -0,0 +1,39 @@
+mi_lista = [1, 2, 3, 4, 5]
+mi_lista2 = ["manzana", "pera", "uva"]  
+
+#len: devuelve el número de elementos de la lista
+mi_lista.len()
+# print(mi_lista.len())
+
+#append: agrega un elemento al final de la lista
+mi_lista2.append("banana")
+# print(mi_lista)
+
+#insert: inserta un elemento en una posición específica
+mi_lista2.insert(0, "kiwi")
+# print(mi_lista2)
+
+#extend: agrega los elementos de una lista a otra lista
+mi_lista2.extend(["kiwi", "manzana"])
+# print(mi_lista2)
+
+#pop: elimina un elemento de la lista por indice
+mi_lista2.pop(0)
+# print(mi_lista2)
+
+#remove: elimina un elemento de la lista por valor
+mi_lista2.remove("kiwi")
+# print(mi_lista2)
+
+#sort: ordena los elementos de la lista en orden ascendente
+mi_lista.sort()
+mi_lista.reverse()
+# print(mi_lista)
+
+#clear: elimina todos los elementos de la lista
+mi_lista2.clear()
+# print(mi_lista2)
+
+
+
+
